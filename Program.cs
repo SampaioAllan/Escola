e8f24f5c962e561403bf9bb490c89e743f288ed5
SampaioAllan <lanbrendo93@gmail.com>
2022-03-29 00:36:22 -0300

    Estruturas do repositório

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Escola
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var secretaria = new Secretaria();
+            secretaria.cadastrarturmas();
+        }
+    }
+}
